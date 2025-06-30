942a5e7ef8558df9e36177c6d2bc753974cfc581
emilyoram <dora458@aucklanduni.ac.nz>
2025-06-30 20:53:05 +1200

    Initial mod set-up

---
+++ src/main/java/com/gmail/sheepingtoninc/rainbowsheep/RainbowSheep.java
@@ -0,0 +1,15 @@
+package com.gmail.sheepingtoninc.rainbowsheep;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(RainbowSheep.MODID)
+public class RainbowSheep {
+    public static final String MODID = "rainbowsheep";
+
+
+    public RainbowSheep(IEventBus modBus, ModContainer container) {
+
+    }
+}
