ec2d7284c481a2866ceafa6b5501b92931bb6899
emilyoram <dora458@aucklanduni.ac.nz>
2025-07-31 21:36:49 +1200

    Loot tables now implemented so sheep drop correct wool on death

--- src/main/java/com/gmail/sheepingtoninc/rainbowsheep/mixin/SheepMixin.java
+++ src/main/java/com/gmail/sheepingtoninc/rainbowsheep/mixin/SheepMixin.java
@@ -1,27 +1,41 @@
 package com.gmail.sheepingtoninc.rainbowsheep.mixin;
 
 import com.gmail.sheepingtoninc.rainbowsheep.api.IFlagSheep;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.storage.loot.LootTable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(Sheep.class)
-public class SheepMixin implements IFlagSheep {
+public abstract class SheepMixin implements IFlagSheep {
+    @Shadow public abstract boolean isSheared();
+
     private static final Logger log = LoggerFactory.getLogger(SheepMixin.class);
     private static final EntityDataAccessor<Integer> FLAG = SynchedEntityData.defineId(Sheep.class, EntityDataSerializers.INT);
+
+    private static final ResourceLocation RAINBOW_LOOT_LOCATION = ResourceLocation.fromNamespaceAndPath("rainbowsheep", "entities/sheep/rainbow");
+    private static final ResourceKey<LootTable> RAINBOW_LOOT_KEY = ResourceKey.create(Registries.LOOT_TABLE, RAINBOW_LOOT_LOCATION);
+
+    private static final ResourceLocation TRANSGENDER_LOOT_LOCATION = ResourceLocation.fromNamespaceAndPath("rainbowsheep", "entities/sheep/transgender");
+    private static final ResourceKey<LootTable> TRANSGENDER_LOOT_KEY = ResourceKey.create(Registries.LOOT_TABLE, TRANSGENDER_LOOT_LOCATION);
 
     private SynchedEntityData getData() {
         return ((Sheep)(Object)this).getEntityData();
@@ -57,5 +71,16 @@ public class SheepMixin implements IFlagSheep {
                     BuiltInRegistries.BLOCK.get(ResourceLocation.fromNamespaceAndPath("rainbowsheep", "transgender_wool"));
             default -> original;
         };
+
+    }
+
+    @Inject(method = "getDefaultLootTable", at = @At(value = "RETURN"), cancellable = true)
+    protected void setFlagLootTable(CallbackInfoReturnable<ResourceKey<LootTable>> cir) {
+        if (!isSheared() && this.getFlagWool() != 0) {
+            switch (this.getFlagWool()) {
+                case 1 -> cir.setReturnValue(RAINBOW_LOOT_KEY);
+                case 2 -> cir.setReturnValue(TRANSGENDER_LOOT_KEY);
+            }
+        }
     }
 }
