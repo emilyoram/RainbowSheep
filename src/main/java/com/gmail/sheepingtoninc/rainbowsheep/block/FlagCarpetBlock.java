8b6c37ab2d809ccccc05046c3bcb27d958e65fc4
emilyoram <dora458@aucklanduni.ac.nz>
2025-08-03 12:59:15 +1200

    Made FlagCarpetBlock less of a mess

--- src/main/java/com/gmail/sheepingtoninc/rainbowsheep/block/FlagCarpetBlock.java
+++ src/main/java/com/gmail/sheepingtoninc/rainbowsheep/block/FlagCarpetBlock.java
@@ -5,18 +5,15 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
-public class FlagCarpetBlock extends DirectionalBlock {
+public class FlagCarpetBlock extends CarpetBlock {
 
     public static final MapCodec<FlagCarpetBlock> CODEC = simpleCodec(FlagCarpetBlock::new);
-    protected static final VoxelShape SHAPE = Block.box(0.0, 0.0, 0.0, 16.0, 1.0, 16.0);
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public FlagCarpetBlock(Properties p_152915_) {
         super(p_152915_);
@@ -25,44 +22,13 @@ public class FlagCarpetBlock extends DirectionalBlock {
     }
 
     @Override
-    protected MapCodec<? extends DirectionalBlock> codec() {
+    public MapCodec<FlagCarpetBlock> codec() {
         return CODEC;
     }
-
-    //All three of these methods directly taken from Carpet Block
-    @Override
-    protected VoxelShape getShape(BlockState p_152917_, BlockGetter p_152918_, BlockPos p_152919_, CollisionContext p_152920_) {
-        return SHAPE;
-    }
-
-    @Override
-    protected BlockState updateShape(
-            BlockState p_152926_, Direction p_152927_, BlockState p_152928_, LevelAccessor p_152929_, BlockPos p_152930_, BlockPos p_152931_
-    ) {
-        return !p_152926_.canSurvive(p_152929_, p_152930_)
-                ? Blocks.AIR.defaultBlockState()
-                : super.updateShape(p_152926_, p_152927_, p_152928_, p_152929_, p_152930_, p_152931_);
-    }
-
-    @Override
-    protected boolean canSurvive(BlockState p_152922_, LevelReader p_152923_, BlockPos p_152924_) {
-        return !p_152923_.isEmptyBlock(p_152924_.below());
-    }
-    //Now back to our regularly scheduled program
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
-    }
-
-    @Override
-    protected BlockState rotate(BlockState state, Rotation rotation) {
-        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
-    }
-
-    @Override
-    protected BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
     }
 
     @Override
@@ -84,5 +50,4 @@ public class FlagCarpetBlock extends DirectionalBlock {
     public int getFireSpreadSpeed(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return 60;
     }
-
 }
